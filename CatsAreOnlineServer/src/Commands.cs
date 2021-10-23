292242518c5193f15364cbf55de0f1e7f93e44ad
ConfiG <cgytrus@cgyt.ru>
2021-10-23 21:45:51 +0300

    add stop command

--- CatsAreOnlineServer/src/Commands.cs
+++ CatsAreOnlineServer/src/Commands.cs
@@ -9,6 +9,15 @@ namespace CatsAreOnlineServer {
         
         // ReSharper disable once ArrangeMethodOrOperatorBody
         public static void Initialize() {
+            dispatcher.Register(LiteralArgumentBuilder<Player>.Literal("stop")
+                .Executes(context => {
+                    if(context.Source != null) {
+                        Server.SendChatMessage(context.Source, Server.ServerErrorMessage($"Not enough permissions"));
+                        return 1;
+                    }
+                    Server.Stop();
+                    return 1;
+                }));
             dispatcher.Register(LiteralArgumentBuilder<Player>.Literal("info")
                 .Executes(context => {
                     Server.SendChatMessage(context.Source, Server.ServerMessage($"<b>INFO:</b> v{Server.Version}"));
